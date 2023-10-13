36aa636a7e64e8f16e92da2819257689fc7c61fc
Olico214 <oliverromero73@gmail.com>
2023-10-13 14:24:41 -0600

    changes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const flowEspecialistas = addKeyword('especialista').addAction(async(ctx,{flowDy
   const es = myState.especialidad
   const doctores = await getDoctor(es,city)
   let especial = "👩🏻‍⚕‍ 👨🏻‍⚕‍ Tenemos a los siguientes Ginecólogos:\n\n";
-  especial += `👉  Escribe el código (las letras en negritas y minúsculas,  Ej. *1* ) del médico para ver su información y poder agendar tu cita:`
+  especial += `👉  Escribe el código (las letras en negritas y minúsculas,  Ej. *1* ) del médico para ver su información y poder agendar tu cita:\n\n\n`
 // Itera a través de los datos de los médicos
 for (let i = 0; i < doctores.length; i++) {
   const doctor = doctores[i];
@@ -43,7 +43,7 @@ for (let i = 0; i < doctores.length; i++) {
   });
 
   // Agrega una línea al mensaje a mostrar
-  especial += `🏥 » *${indice}*: ${doctor.nameDoc}\n${doctor.EspecialidadCompleta} - ${doctor.HospitalTorre}\n\n`;
+  especial += `\n\n🩺 » *${indice}*: ${doctor.nameDoc}\n${doctor.EspecialidadCompleta} - ${doctor.HospitalTorre}\n\n`;
 }
 
 
