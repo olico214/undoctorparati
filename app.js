c29625360fca6a25139919d9eaf68055aab44c49
olico214 <oliverromero73@gmail.com>
2023-10-14 09:01:07 -0600

    changes

--- app.js
+++ app.js
@@ -27,7 +27,14 @@ flowDynamic({body:`👌 ¡ Muchas gracias ${datosPaciente.nombrePaciente}!\n〰�
 
 const flowValidate = addKeyword('validate').addAction((ctx,{flowDynamic,gotoFlow,endFlow,state})=>{
   const estatus = state.getMyState()
-  const correo = (estatus.email = 0 ? 'Sin correo':estatus.email)
+  let correo = "";
+
+  if(estatus.email == 0 ){
+    correo = "Sin Correo"
+  }else{
+    correo = estatus.email;
+  }
+
   state.update({email:correo})
   flowDynamic({body:`Motivo:${estatus.motivo}\nNombre Paciente: ${estatus.nombrePaciente}\nCorreo: (${correo})`})
 })
